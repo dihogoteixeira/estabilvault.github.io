17d85bbc01d9e346c03f592fbe38787bd2b751a7
Dihogo Cassimiro Teixeira <diteixeira@MacBook-Pro.local>
2022-11-16 22:41:12 -0300

    website

--- docs/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ...require('@hashicorp/platform-cli/config/.eslintrc'),
-  /* Specify overrides here */
-  ignorePatterns: ['public/']
-}
--- docs/redirects.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  // example redirect:
-  // {
-  //   source: '/vault/docs/some/path',
-  //   destination: '/vault/docs/some/other/path',
-  //   permanent: true,
-  // },
-]
